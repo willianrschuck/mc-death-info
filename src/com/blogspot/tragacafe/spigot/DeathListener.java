927556942b8a99078ed3c61b4512f87c3622c6d6
Willian Schuck <willianrschuck@gmail.com>
2020-08-12 21:33:32 -0300

    Alterada a cor da mensagem para vermelho. O nome da dimensão é exibido caso não seja o Overworld.

--- src/com/blogspot/tragacafe/spigot/DeathListener.java
+++ src/com/blogspot/tragacafe/spigot/DeathListener.java
@@ -1,7 +1,8 @@
 package com.blogspot.tragacafe.spigot;
 
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World.Environment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -12,9 +13,30 @@ public class DeathListener implements Listener {
 	public void onPlayerDie(PlayerDeathEvent event) {
 		
 		Location playerDeathLocalization = event.getEntity().getLocation();
+		Environment environment = playerDeathLocalization.getWorld().getEnvironment();
 		int x = playerDeathLocalization.getBlockX();
 		int z = playerDeathLocalization.getBlockZ();
-		event.getEntity().sendMessage(Color.WHITE + "Você morreu nas coordenadas x: " + x + " z: " + z);
+		
+		String msg = ChatColor.DARK_RED + "Você morreu em x: " + x + " z: " + z;
+		
+		if (environment != Environment.NORMAL) {
+			msg += " no " + getDimensionName(environment); 
+		}
+		
+		event.getEntity().sendMessage(msg);
+		
+	}
+
+	private String getDimensionName(Environment environment) {
+
+		switch (environment) {
+		case NETHER:
+			return "Nether";
+		case THE_END:	
+			return "The End";
+		default:
+			return "Overworld";
+		}
 		
 	}
 	
